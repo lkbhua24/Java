87e614a2fa7f261bb271abeffdd57ea22acb9acb
莱克瓦 <2711627633@qq.com>
2025-09-18 22:13:49 +0800

    Java入门
    变量数据类型基本概念

---
+++ ValueDemo1.java
@@ -0,0 +1,31 @@
+﻿public class ValueDemo1{
+	public static void main(String[] args){
+		System.out.println("Hello World!你好！");
+		// 目标：需要大家掌握常见的数据在代码中如何书写的？
+		
+		// 整数
+		System.out.println(666);
+		System.out.println(-77);
+		
+		// 小数
+		System.out.println(3.14);
+		System.out.println(-0.24);
+		
+		// 字符串
+		System.out.println("莱克瓦");
+		System.out.println("Java学习之路");
+		
+		// 字符
+		System.out.println("男");
+		
+		// 布尔
+		System.out.println(true);
+		System.out.println(false);
+		
+		// 空
+		// 细节：null不能直接被打印
+		// 如果我们要打印null，那么只能用字符串的形式进行打印
+		System.out.println("null");
+		
+	}
+}
