b88ac3409e91511261dd64068b0ef08a663e3ce4
莱克瓦 <2711627633@qq.com>
2025-09-19 10:12:41 +0800

    Java入门
    不同数据类型输出

---
+++ VariableDemo3.java
@@ -0,0 +1,40 @@
+public class VariableDemo3{
+	// 主入口
+	public static void main(String[] args){
+		// 1.byte
+		byte b =10;
+		System.out.println(b);
+		
+		// short
+		short s = 20;
+		System.out.println(s);
+		
+		// int
+		int i = 30;
+		System.out.println(i);
+		
+		// long
+		// 如果要定义long类型的变量
+		// 在数据值的后面需要加一个L作为后缀
+		// 可以是大写的也可以是小写的
+		// 建议：使用大写。
+		long n = 99999999L;
+		System.out.println(n);
+		
+		// float
+		float f = 10.1F;
+		System.out.println(f);
+		
+		// double
+		double d= 20.2;
+		System.out.println(d);
+		
+		// char
+		char c = 'a';
+		System.out.println(c);
+		
+		// boolean
+		boolean o =true;
+		System.out.println(o);
+	}
+}
