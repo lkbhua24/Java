c79c2f4da06d1cb7ae4846d27e4af7802a4bfb47
莱克瓦 <2711627633@qq.com>
2025-09-20 14:15:52 +0800

    Java入门
    应用练习2

---
+++ VariableTest2.java
@@ -0,0 +1,27 @@
+public class VariableTest2{
+	// 主入口
+	public static void main(String[] args){
+		// 定义变量记录姓名
+		String name = "lkbhua";
+		
+		// 定义变量记录年龄
+		int age = 20;
+		
+		// 定义变量记录性别
+		char gender = '男';
+		
+		// 定义变量记录身高
+		double height = 178.1;
+		
+		// 定义变量记录是否单身
+		boolean flag = true;
+		
+		// 输出变量的值
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(gender);
+		System.out.println(flag);
+		System.out.println(height);
+		
+	}
+}
