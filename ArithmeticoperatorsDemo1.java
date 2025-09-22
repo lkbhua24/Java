62dd759bd5cb4d7fa6a95a0f77dcd43b7c772e61
莱克瓦 <2711627633@qq.com>
2025-09-22 16:32:45 +0800

    Java基础
    算术运算符基础知识与应用

---
+++ ArithmeticoperatorsDemo1.java
@@ -0,0 +1,38 @@
+package com.lkbhua.operators;
+
+/*
+public class ArithmeticoperatorsDemo1 {
+    public static void main(String[] args) {
+        // +
+        System.out.println(3+2);
+        // -
+        System.out.println(3-2);
+        // *
+        System.out.println(3*2);
+
+        // 如果在计算中的时候有小数参与
+        // 结论：
+        // 在代码中，如果有小数参与计算，结果可能不精确的。
+        System.out.println(1.1+2.1);
+        System.out.println(1.1+1.10);
+        System.out.println(1.1-1.01);
+
+        // /
+        // 整数参与计算，结果只能是整数
+        // 小数参与计算，结果可能不精确。
+        System.out.println(10/2);
+        System.out.println(10/3);
+        System.out.println(10.0/3);
+
+        // %
+        System.out.println(10%2);
+        System.out.println(10%3);
+
+        // 应用场景
+        // 1.可以用取模判断，A是否可以被B整除
+        // A%B
+        // 2.可以判断A是否是偶数
+        // 结果为0则是偶数，为1则是奇数！
+    }
+}
+*/
