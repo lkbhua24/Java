c6a39c2b18e357c870baf07a3997901432ef7007
莱克瓦 <2711627633@qq.com>
2025-09-22 16:33:23 +0800

    Java基础

---
+++ ArithmeticoperatorsDemo2.java
@@ -0,0 +1,14 @@
+package com.lkbhua.operators;
+/*
+public class ArithmeticoperatorsDemo2 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+        int sum = a+b;
+        System.out.println(sum);
+        // £¨byte£©b1+b2 Ç¿ÖÆ×ª»»
+        byte result = (byte)(a+b);
+        System.out.println(result);
+    }
+}
+*/
