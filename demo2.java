1e82ef3d8beaaaaa890c5600d299656bea72afc7
莱克瓦 <2711627633@qq.com>
2025-09-28 19:51:19 +0800

    Java基础
    Java基础--for循环语句-1-100之间的偶数和计算

---
+++ demo2.java
@@ -0,0 +1,16 @@
+package com.lkbhua.loopdemo;
+
+public class demo2 {
+    public static void main(String[] args)
+    {
+        /* 需求：求1-100之间的偶数和
+        * */
+        int sum =0;
+        for(int i=1;i<=100;i++){
+            if(i%2==0){
+                sum += i;
+                System.out.println("sum="+sum);
+            }
+        }
+    }
+}
