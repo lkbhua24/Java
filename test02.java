35aef734ff988cd4c5a7414f85c492393bcfd366
莱克瓦 <2711627633@qq.com>
2025-09-30 08:39:56 +0800

    Java基础练习
    Java基础练习2——多分支if语句

--- test02.java
+++ test02.java
@@ -1,28 +1,37 @@
-package com.lkbhua.operators.com.lkbhua;
+package com.lkbhua.Test;
 
 import java.util.Scanner;
 
 public class test02 {
     public static void main(String[] args){
-        /* 需求:
-        您和您的约会对象正试图在餐厅的一张桌子吃饭
-        键盘录入两个整数，表示你和你的约会对象的衣服时髦程度（手动录入0-10的整数，不能是其他）
-        如果你的时髦程度大于你的约会对象的时髦程度，则相亲成功输出true
-        否则输出false
+        /*  需求：输入一个年龄。
+        判断是儿童还是少年还是青年还是中年还是老年还是神仙
+        儿童（0-11）
+        少年（12-17）
+        青年（18-30）
+        中年（31-50）
+        老年（51-70）
         * */
-
-        // 1。键盘录入两个整数表示衣服时髦程度
+        System.out.println("请输入年龄：");
         Scanner sc = new Scanner(System.in);
-        System.out.println("请输入你的时髦程度：");
-        int myLevel = sc.nextInt();
-        System.out.println("请输入你的约会对象的时髦程度：");
-        int yourLevel = sc.nextInt();
-
-        // 2.把我衣服的时髦程度跟女孩的时髦程度进行比较
-        boolean result = myLevel > yourLevel;
-
-        // 3.打印结果
-        System.out.println(result);
+        int age = sc.nextInt();
+        if(age>=0 && age<=11){
+            System.out.println("儿童");
+        }
+        else if(age>=12 && age<=17){
+            System.out.println("少年");
+        }
+        else if(age>=18 && age<=30){
+            System.out.println("青年");
+        }
+        else if(age>=31 && age<=50){
+            System.out.println("中年");
+        }
+        else if(age>=51 && age<=70){
+            System.out.println("老年");
+        }
+        else{
+            System.out.println("神仙");
+        }
     }
-
 }
