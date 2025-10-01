47b82d1a4476b7e0fdca6a94e0b3f26d4dc6514f
莱克瓦 <2711627633@qq.com>
2025-10-01 09:38:25 +0800

    Java基础练习
    Java基础练习3——If语句的应用

--- test03.java
+++ test03.java
@@ -1,28 +1,36 @@
-package com.lkbhua.operators.com.lkbhua;
+package com.lkbhua.Test;
 
 import java.util.Scanner;
 
 public class test03 {
     public static void main(String[] args) {
-        /*
-        键盘输入两个整数，如果其中一个为6，最终结果输出true
-        如果它们的和为6的倍数，最终结果输出true
-        其他情况都是false
-        */
-        // 分析：
-        // 1.键盘输入两个整数
-        // 变量a   变量b
-
-        // 2.a == 6 || b == 6 || a + b == 6
-        // 如果满足其中的一个则输出true
-
+        /*  需求：商城都会有vip会员制度，假设商品为1000元
+        键盘录入会员级别，并计算出实际支付的钱
+        会员1级，打9折
+        会员2级，打8折
+        会员3级，打7折
+        非会员，不打折
+        * */
+        int price = 1000;
+        System.out.println("请输入会员级别：");
         Scanner sc = new Scanner(System.in);
-        System.out.println("请输入第一个整数：");
-        int a = sc.nextInt();
-        System.out.println("请输入第二个整数：");
-        int b = sc.nextInt();
-        boolean result = a == 6 || b == 6 || a + b == 6;
-        System.out.println(result);
-
+        int level = sc.nextInt();
+        if(level == 1)
+        {
+            System.out.println("会员1级，打9折，实际支付：" + price * 0.9);
+        }
+        else if(level == 2)
+        {
+            System.out.println("会员2级，打8折，实际支付：" + price * 0.8);
+        }
+        else if(level == 3)
+        {
+            System.out.println("会员3级，打7折，实际支付：" + price * 0.7);
+        }
+        else
+        {
+            System.out.println("非会员，不打折，实际支付：" + price);
+        }
+        System.out.println("欢迎下次光临");
     }
 }
