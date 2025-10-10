a8b1d0c8f66ca1194b07c4a4ceb526f05a54b458
莱克瓦 <2711627633@qq.com>
2025-10-10 10:18:54 +0800

    Java基础
    Java基础——方法的定义与应用

--- demo1.java
+++ demo1.java
@@ -1,13 +1,17 @@
-package com.lkbhua.InfiniteloopDemo1;
+package com.lkbhua.MethodDemo1;
 
 public class demo1 {
     public static void main(String[] args) {
-
-        // 注意事项：
-        // 1. 循环条件永远为true
-        // 2.无线循环的下面不能再写其他的代码了，因为执行不到！
-        while (true) {
-            System.out.println("hello world");
-        }
+        /*  方法的定义和调用
+        * */
+        // 调用方法
+        playgame();
+    }
+    // 定义一个方法
+    public static void playgame() {
+        System.out.println("开始游戏");
+        System.out.println("打怪兽");
+        System.out.println("打 boss");
+        System.out.println("结束游戏");
     }
 }
