22e2ed9ff5bf328bf9d866db4e0ff15f7764eb91
莱克瓦 <2711627633@qq.com>
2025-10-10 10:19:22 +0800

    Java基础
    Java基础——方法的定义与应用2

--- demo2.java
+++ demo2.java
@@ -1,50 +1,15 @@
-package com.lkbhua.switchdemo;
-
-import java.util.Scanner;
+package com.lkbhua.MethodDemo1;
 
 public class demo2 {
     public static void main(String[] args) {
-        /*  需求：键盘录入星期数，输出工作日和休息日
+        /*  需求：掌握带参数方法定义和调用
+        *   定义一个方法，求长方形的周长，将结果在方法中打印
         * */
-        int day=0;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入星期数：");
-        day = sc.nextInt();
-        switch (day){
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("工作日");
-                break;
-            case 6:
-            case 7:
-                System.out.println("休息日");
-                break;
-            default:
-                System.out.println("输入的星期数有误");
-        }
+        getPerimeter(5,10);
     }
-}
+    public static void getPerimeter(double a,double b){
+        double perimeter = 2*(a+b);
+        System.out.println(perimeter);
+    }
 
-/*
-还可以简化
-switch (day){
-case 1，2，3，4，5:
-    System.out.println("工作日");
-    break;
-case 6，7:
-    System.out.println("休息日");
-    break;
-default:
-        System.out.println("输入的星期数有误");
-        break;
 }
-还可以简化
-switch (day){
-case 1，2，3，4，5-> System.out.println("工作日");
-case 6，7-> System.out.println("休息日");
-default-> System.out.println("输入的星期数有误");
-}
-* */
