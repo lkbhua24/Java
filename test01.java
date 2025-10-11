4fec43bc4cdc97444474c83ffda5f5368ea7a308
莱克瓦 <2711627633@qq.com>
2025-10-11 09:10:17 +0800

    Java基础练习
    Java基础练习——方法练习

--- test01.java
+++ test01.java
@@ -2,26 +2,15 @@ package com.lkbhua.Test;
 
 public class test01 {
     public static void main(String[] args) {
-        /*  需求：逢7过
-        *   当你要报的数字是7的倍数或者包含7的时候，就输出7过
-        *   请你输出0-100之间的逢7过数字
+        /*  需求：定义一个方法，在方法内部定义两个变量
+                求出他们的和并进行打印
         * */
-        // 分析：
-        // 出现7的情况有 个位是7，十位是7，百位是7 7的倍数
-        // 7 14 21 28 35 42 49 56 63 70 77 84 91
-
-        // 判断：
-        // 个位是7：xxx%10==7？
-        // 十位是7：xxx/10%10==7？
-        // 百位是7：xxx/100%10==7？
-        // 7的倍数：xxx%7==0？
-        for (int i = 0; i < 100; i++) {
-            if (i % 10 == 7 || i / 10 % 10 == 7 || i % 7 == 0) {
-                System.out.println("7过");
-                continue;
-            }
-            System.out.println(i);
-        }
+        add(10,20);
     }
 
+    public static void add(int a,int b)
+    {
+        int sum = a+b;
+        System.out.println(sum);
+    }
 }
