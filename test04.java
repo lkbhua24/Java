85db11e925c632e8453bf425053b21decd15e672
莱克瓦 <2711627633@qq.com>
2025-10-12 14:36:20 +0800

    Java基础练习
    Java基础练习4——允许重载的条件

--- test04.java
+++ test04.java
@@ -1,30 +1,31 @@
 package com.lkbhua.Test;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class test04 {
-    public static void main(String[] args) {
-        /*  需求：猜数字游戏：0-100
+    public static void main(String[] args){
+        /*  需求： 使用方法重载的思想，设计比较两个整数是否相同的方法
+         *        兼容全整数类型（byte、short、int、long）
         * */
-        Random r = new Random();
-        // 小括号内是0-99，包头不包尾
-        int number = r.nextInt(100) + 1;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入你要猜的数字：");
-        int num = sc.nextInt();
-        while (true) {
-            if (num > number) {
-                System.out.println("你猜的数字太大了");
-            }
-            else if (num< number) {
-                System.out.println("你猜的数字太小了");
-            }
-            else{
-                System.out.println("恭喜你猜对了");
-                break;
-            }
-        }
+		// 允许重载的条件:
+		// 方法名必须相同。
+		// 参数列表(数量、类型、顺序)必须不同。
+		// 返回值类型、访问修饰符、异常声明等可以不同，但不是重载的必要条件。
+     compare(5,5);
+     compare(5L,5);
+     compare((byte) 10,(byte) 10);
+     compare((short) 10,(short) 10);
 
+
+    }
+    public static boolean compare(byte a,byte b){
+        return a==b;
+    }
+    public static boolean compare(short a,short b){
+        return a==b;
+    }
+    public static boolean compare(int a,int b){
+        return a==b;
+    }
+    public static boolean compare(long a,long b){
+        return a==b;
     }
 }
