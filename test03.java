b693909bb13d7108783e7b08179ed6241eeaa9c5
莱克瓦 <2711627633@qq.com>
2025-10-12 14:31:53 +0800

    Java基础练习
    Java基础练习3——求长方形面积并且比较大小

--- test03.java
+++ test03.java
@@ -1,32 +1,22 @@
 package com.lkbhua.Test;
 
-import java.util.Scanner;
-
 public class test03 {
-    public static void main(String[] args) {
-        /*  需求：判断一个数是否是质数
-        *   质数：只能被1和自身整除的数
+    public static void main(String[] args){
+        /*  需求：定义一个方法求长方形的面积，并且比较两个长方形的大小
         * */
-        // 判断：
-        // 写一个循环，从2开始，一直判断到num-1
-        // 看这个范围之内，有没有数字可以被num整除
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请输入一个数字：");
-        int num = sc.nextInt();
-        // 定义一个变量标记：标记num是否一个质数
-        boolean flag = true;
-        for(int i = 2; i < num; i++){
-            if(num%i==0){
-                flag = false;
-                // System.out.println("不是质数");
-                break;
-            }
-        }
-        // 只有当循环结束了，表示这个范围之内所有的数字都没有被num整除，那么num就是质数
-        if(flag){
-            System.out.println("是质数");
+        double area1 = getArea(5.0,8.0);
+        double area2 = getArea(3.0,4.0);
+        if(area1 > area2){
+            System.out.println("第一个面积大");
         }else{
-            System.out.println("不是质数");
+            System.out.println("第二个面积大");
         }
+
     }
+    public static double getArea(double length,double width){
+        double area = length * width;
+        System.out.println("面积是：" + area);
+        return area;
+    }
+
 }
