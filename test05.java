c29ec126e87ec4aaee0f3b018b0d9284b0c5f29a
莱克瓦 <2711627633@qq.com>
2025-10-14 23:38:43 +0800

    Java基础练习
    Java基础练习5

--- test05.java
+++ test05.java
@@ -1,30 +1,15 @@
 package com.lkbhua.Test;
 
-import java.util.Scanner;
-
 public class test05 {
-    public static void main(String[] args){
-        /*  需求： 给定两个整数，被除数和除数（都是正数，且不超过int的范围）
-            将两数相除，要求不使用乘法，除法和取余运算，并返回商和余数。
+    public static void main(String[] args) {
+        /*  需求： 定义一个方法用于数组遍历，要求遍历的结果是在一行上的。
         * */
-        // 思路：除法的本质就是计数被除数中有多少次可以被除数相减的倍
-
-        // 提示用户输入两个数
-        System.out.println("请输入被除数和除数:");
-
-        // 获取用户输入
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int count = 0;
-
-        // 利用循环进行相减逻辑
-        while(a>=b){
-            a = a-b;
-            count++;
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        printArray(arr);
+    }
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
         }
-        // 输出结果
-        System.out.println("商为："+count);
-        System.out.println("余数为："+a);
     }
 }
