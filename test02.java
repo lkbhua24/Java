0ccc81e2be0078f6f3f6216dbc100fc78d15f34f
莱克瓦 <2711627633@qq.com>
2025-10-16 15:48:05 +0800

    Java基础综合练习
    Java基础综合练习2——求素数

--- test02.java
+++ test02.java
@@ -1,14 +1,28 @@
-package com.lkbhua.Test;
+package com.lkbhua;
 
 public class test02 {
     public static void main(String[] args) {
-        /*  需求：求利用方法一个圆的面积，并进行打印
+        /*  需求：判断101-200之间有多少个素数，并输出所有素数
+            素数：只能被1和自身整除的数
         * */
-        getArea(5.0);
-    }
-
-    public static void getArea(double radius){
-        double area = 3.14 * radius * radius;
-        System.out.println(area);
+        // 循环嵌套
+        // 思路一：
+        // 1. 遍历101-200
+        int count = 0;
+        for (int i = 101; i <= 200; i++) {
+            // 2. 判断素数
+            boolean flag = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    flag = false;
+                    // 跳出内循环，执行下面代码
+                    break;
+                }
+                if (flag) {
+                    System.out.println("当前数字" + i + "是素数");
+                    count++;
+                }
+            }
+        }
     }
 }
