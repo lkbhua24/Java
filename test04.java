cc2f4eefd7e0d8b548f224353f4fde13ac6adcd0
莱克瓦 <2711627633@qq.com>
2025-10-17 15:28:57 +0800

    Java基础综合练习
    Java基础综合练习5——复制数组

--- test04.java
+++ test04.java
@@ -1,31 +1,24 @@
-package com.lkbhua.Test;
+package com.lkbhua;
 
 public class test04 {
-    public static void main(String[] args){
-        /*  需求： 使用方法重载的思想，设计比较两个整数是否相同的方法
-         *        兼容全整数类型（byte、short、int、long）
-        * */
-		// 允许重载的条件:
-		// 方法名必须相同。
-		// 参数列表(数量、类型、顺序)必须不同。
-		// 返回值类型、访问修饰符、异常声明等可以不同，但不是重载的必要条件。
-     compare(5,5);
-     compare(5L,5);
-     compare((byte) 10,(byte) 10);
-     compare((short) 10,(short) 10);
+    public static void main(String[] args) {
+        /*  需求：把一个数组中的元素复制到另一个新数组中去
+         * */
+         // 分析：
+         // 定义一个数组
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
 
+        // 2.定义一个新数组
+        int[] newArr = new int[arr.length];
 
-    }
-    public static boolean compare(byte a,byte b){
-        return a==b;
-    }
-    public static boolean compare(short a,short b){
-        return a==b;
-    }
-    public static boolean compare(int a,int b){
-        return a==b;
-    }
-    public static boolean compare(long a,long b){
-        return a==b;
+        // 3.遍历数组，把数组中的元素依次放到新数组中
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+
+        // 4.输出新数组
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.println(newArr[i]);
+        }
     }
 }
