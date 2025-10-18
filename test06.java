dd11a951480c824c09c6a9f09ef7cb02d5e57bad
莱克瓦 <2711627633@qq.com>
2025-10-18 23:42:49 +0800

    Java基础综合练习
    Java基础综合练习6

--- test06.java
+++ test06.java
@@ -1,20 +1,41 @@
-package com.lkbhua.Test;
+package com.lkbhua;
 
 public class test06 {
     public static void main(String[] args) {
-        /*  需求：定义一个方法，用以求数组的最大值
+        /*  需求：某系统的数组密码（大于0），比如1983，采用加密方式进行传输
+        规则如下： 先得到每位数，然后每位数都加上5，再对10取余，最后将每位数进行反转，得到一串新数
         * */
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        int max = getMax(arr);
-        System.out.println(max);
-    }
-    public static int getMax(int[] arr){
-        int max = arr[0];
+        // 分析：
+        // 1.把整数里面的每一位放到数组当中
+        int []arr = {1,9,8,3};
+
+        // 2.加密
+        // ①每一位加上5
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i]>max){
-                max = arr[i];
-            }
+            arr[i]+=5;
         }
-        return max;
+
+        // ②对10取余
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]%=10;
+        }
+
+        // ③反转
+        for (int i = 0,j=arr.length-1; i < j; i++,j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+
+        // 3，把数组里面的每一个数字进行拼接，变成加密之后的结果
+        int number = 0;
+        for (int i = 0; i < arr.length; i++) {
+            number = number * 10 + arr[i];
+            System.out.print(number);
+        }
+
+
+
     }
 }
