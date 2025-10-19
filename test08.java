6308ae42d70289e468d92d9e302ec3d93be54b69
莱克瓦 <2711627633@qq.com>
2025-10-19 10:08:04 +0800

    Java基础综合练习
    Java基础综合练习8——解密过程

--- test08.java
+++ test08.java
@@ -1,34 +1,39 @@
-package com.lkbhua.Test;
+package com.lkbhua;
 
 public class test08 {
     public static void main(String[] args) {
-        /*    需求：定义一个方法copyOfRange(int[] arr,int start,int end)，
-        *      功能：将指定数组arr中，start开始，end结束的元素拷贝到一个新的数组中，并返回该数组
+        /*  需求 ： 解密
         * */
-        // 1.定义一个原始数组
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        // 2。调用方法拷贝数据
-        int [] copyArr = copyOfRange(arr,3,7);
+        int [] arr = {8,3,4,6};
 
-        // 3.打印新数组
-        for (int i = 0; i < copyArr.length; i++) {
-            System.out.print(copyArr[i]+" ");
+        // 1.反转
+        for (int i = 0,j = arr.length - 1; i < j; i++,j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
-    }
-    public static int[] copyOfRange(int[] arr,int start,int end) {
-        // 1.定义一个新数组
-        // 不知道所有的元素，用动态数组
-        int [] newArr = new int[end-start];
 
-        // 伪造索引思想
-        int index = 0;
-        // 2.把原始数组中的数据拷贝到新数组中
-        for (int i = start; i < end; i++) {
-            // 格式：newArr[i-start] = arr[i];
-            newArr[index] = arr[i];
-            index++;
+
+        // 2.由于加密是通过对10取余的方式进行取余的
+        // 所以在解密的时候就需要判断，0-4之间+10，5-9之间数字不变
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] >= 0 && arr[i] <= 4){
+                arr[i] = arr[i] + 10;
+            }
         }
-        // 3.返回新数组
-        return newArr;
+
+
+        // 3.每一位减5
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = arr[i] - 5;
+        }
+
+        // 4.获取每一位数字
+        int number = 0;
+        for(int i = 0; i < arr.length; i++){
+            number = number * 10 + arr[i];
+        }
+        System.out.println(number);
+
     }
 }
