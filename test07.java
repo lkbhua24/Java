914c95a15159c092ce8d7c1ebeae8e64ee8e406d
莱克瓦 <2711627633@qq.com>
2025-10-19 10:07:24 +0800

    Java基础综合练习
    Java基础综合练习7——取数进组

--- test07.java
+++ test07.java
@@ -1,28 +1,48 @@
-package com.lkbhua.Test;
-import java.util.Scanner;
+package com.lkbhua;
+
 public class test07 {
     public static void main(String[] args) {
-        /*  需求：定义一个方法判断数组中的某一个数是否存在，将结果返回给调用者
-        * */
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        boolean result = find(arr,10);
-        System.out.println(result);
-    }
-    // 分析：
-    // 1.你要干嘛？              判断数字是否在数组中
-    // 2.需要怎么样才能完成？      需要数组、待查找的数字
-    // 3，调用处是否返回结果？     返回true/false
-    public static boolean find(int [] arr,int number){
-        for (int i = 0; i < arr.length; i++) {
-            int j = arr[i];
-            if (j == number) {
-                return true;
-            }
+        /*  需求： 把整数上的每一位都添加到数组当中
+            整数： 123 12314，反正大于0
+        * * */
+        // 反向推导
+
+
+        // 3.计算出数组的长度
+        int number =12345;
+
+        // 定义一个变量
+        int count = 0;
+        // 定义一个临时变量，存储原来的数字，以便后续使用
+        int temp = number;
+        while(number!=0){
+            // 每一次循环去掉右边的一个数字
+            number = number/10;
+            System.out.println(number);
+            // 去掉一次，计数器+1
+            count++;
         }
-        return false;
+
+        // 2.需要有一个数组
+        // 动态初始化
+        int[] arr = new int[count];
+
+        // 1.把整数上的每一位都添加到数组当中
+        int index = arr.length-1;
+        while(temp!=0){
+            // 获取temp中的每一位数字
+            int ge = temp%10;
+            // 把当前获得的个位添加到数组里面去
+            arr[index] = ge;
+            // 再去掉右边的数字
+            temp = temp/10;
+            index--;
+        }
+
+        // 4.验证数组
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+
     }
 }
-// return break 的区别
-// return：其实跟循环没有什么关系，跟方法有关的，表示1结束方法，2返回结果
-//        如果方法执行到了return，那么方法就会结束，循环也会随之结束。
-// break：跟循环有关，表示结束循环，还会执行循环外的方法内的后续代码。
